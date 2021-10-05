aaa6d8a5ac2d54f4b99877b3492fbc8b355cde16
Jerome <jerome525@gmail.com>
2021-10-05 16:55:25 -0600

    adding a struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+struct Hex {
+    pub elevation: u8,
+    pub occupied: bool,
+}
+
+impl Hex {
+    pub fn new() -> Hex {
+        Hex { elevation: 0, occupied: false, }
+    }
+}
+
+struct Board {
+    
+}
+
 fn main() {
     println!("Hello, world!");
+    let hex = Hex::new();
 }
