4ed3b77ac165781c10aa8c26df15f53968838197
Jerome <jerome525@gmail.com>
2021-10-05 22:18:35 -0600

    started adding basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,59 @@
 
 struct Hex {
-    pub elevation: u8,
+    pub elevation: i8,
     pub occupied: bool,
+    location_x: u8,
+    location_y: u8,
+}
+/*
++ 0 1 2 3 4 5 6
+0 - - - * * * *
+1 - - * * * * *
+2 - * * * * * *
+3 * * * * * * *
+4 * * * * * * -
+5 * * * * * - -
+6 * * * * - - -
+*/
+fn make_board(size: u8) -> Vec<Vec<Hex> > {
+    let height = size;
+    let width = size;
+    let mut board: Vec<Vec<Hex> > = Vec::new();
+    {
+        let mut row: Vec<Hex> = Vec::new();
+        for x in 0..width {
+            row.push(Hex { elevation: 1, occupied: true, location_x: x, location_y: 0, });
+        }
+        board.push(row);
+    }
+    for y in 1..height - 1 {
+        let mut row: Vec<Hex> = Vec::new();
+        for x in 0..width {
+            row.push(Hex { elevation: 0, occupied: false, location_x: x, location_y: y, });
+        }
+        board.push(row);
+    }
+    {
+        let mut row: Vec<Hex> = Vec::new();
+        for x in 0..width {
+            row.push(Hex { elevation: -1, occupied: true, location_x: x, location_y: height, });
+        }
+        board.push(row);
+    }
+    board
 }
 
-impl Hex {
-    pub fn new() -> Hex {
-        Hex { elevation: 0, occupied: false, }
+fn move_elevation(from: Hex, to: Hex) -> ( Hex, Hex ) {
+    if from.occupied || to.occupied {
+        (from, to)
+    } else {
+        (from, to)
     }
 }
 
-struct Board {
-    
-}
-
 fn main() {
+    let board = make_board(7);
+    
+
     println!("Hello, world!");
-    let hex = Hex::new();
 }
