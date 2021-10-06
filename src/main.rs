16bf69b73f016e50ef2fa6a10b5c3550dcf1ecb5
Jerome <jerome525@gmail.com>
2021-10-06 12:41:55 -0600

    added some checks

--- src/main.rs
+++ src/main.rs
@@ -43,17 +43,30 @@ fn make_board(size: u8) -> Vec<Vec<Hex> > {
     board
 }
 
-fn move_elevation(from: Hex, to: Hex) -> ( Hex, Hex ) {
-    if from.occupied || to.occupied {
-        (from, to)
-    } else {
-        (from, to)
+fn check_in_bounds(to_check: &Hex) -> bool {
+   let x_plus_y = to_check.location_x + to_check.location_y; 
+   if x_plus_y > 9 || x_plus_y < 3 {
+       return false;
+   }
+   true
+}
+
+fn move_elevation(from: Hex, to: Hex) -> (Hex, Hex, bool) {
+    if !from.occupied && !to.occupied && from.elevation > -1 && to.elevation < 1 && check_in_bounds(&from) && check_in_bounds(&to) {
+        return (
+            Hex { elevation: from.elevation - 1 , occupied: false, location_x: from.location_x, location_y: from.location_y },
+            Hex { elevation: to.elevation + 1 , occupied: false, location_x: to.location_x, location_y: to.location_y },
+            true,
+        )
     }
+    (from, to, false)
 }
 
 fn main() {
     let board = make_board(7);
-    
-
+    let spot1 = Hex { elevation: 1, occupied: false, location_x: 6, location_y: 0 };
+    let spot2 = Hex { elevation: 1, occupied: false, location_x: 4, location_y: 4 };
+    let (spot3, spot4, success) = move_elevation(spot1, spot2);
+    println!("{}", success);
     println!("Hello, world!");
 }
